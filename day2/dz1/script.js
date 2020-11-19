f71f574ff6e6164ee4cb771863c2b4a0eb78107b
Mikle Seregin <mikhail-seregin@yandex.ru>
2020-11-19 15:49:41 +0300

    day2

--- day2/dz1/script.js
+++ day2/dz1/script.js
@@ -1,45 +1,45 @@
 window.onload = function () {
-  let form =  document.querySelector('form');
-  let inputs = document.querySelectorAll('input');
-  form.addEventListener('submit', validate);
+    let form = document.querySelector('form');
+    let inputs = document.querySelectorAll('input');
+    form.addEventListener('submit', validate);
 
-  function validate() {
-      let error = false;
+    function validate() {
+        let error = false;
 
 
-      for (let i=0; i< inputs.length;i++){
-          let field = inputs[i];
+        for (let i = 0; i < inputs.length; i++) {
+            let field = inputs[i];
 
-          if (!checkField(field)){
-              field.classList.add('error');
-              error = true;
-          }
-      }
+            if (!checkField(field)) {
+                field.classList.add('error');
+                error = true;
+            }
+        }
 
-      if (error){
-          event.preventDefault();
-      } else {
-          alert('Валидация успешна');
-      }
+        if (error) {
+            event.preventDefault();
+        } else {
+            alert('Валидация успешна');
+        }
 
-  }
+    }
 
-    for (let i=0; i< inputs.length;i++){
+    function checkField(field) {
+
+        let val = field.value;
+        let type = field.type;
+
+        if (type == 'text') {
+            return val.length > 0;
+        } else {
+            return val.length > 5;
+        }
+    }
+
+    for (let i = 0; i < inputs.length; i++) {
         let field = inputs[i].oninput = function (e) {
             this.classList.remove('error');
         };
     }
-
-  function checkField(field) {
-
-      let val = field.value;
-      let type = field.type;
-
-      if (type=='text'){
-          return val.length>0;
-      } else {
-          return val.length>5;
-      }
-  }
 
 };
