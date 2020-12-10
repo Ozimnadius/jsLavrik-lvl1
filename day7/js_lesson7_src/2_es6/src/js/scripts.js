0971b30b7f44209086cf0f89ef3fe55c4fea688b
Mikle Seregin <mikhail-seregin@yandex.ru>
2020-12-10 16:55:53 +0300

    End

--- day7/js_lesson7_src/2_es6/src/js/scripts.js
+++ day7/js_lesson7_src/2_es6/src/js/scripts.js
@@ -1,15 +1,36 @@
 "use strict";
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+var _createClass = function () {
+    function defineProperties(target, props) {
+        for (var i = 0; i < props.length; i++) {
+            var descriptor = props[i];
+            descriptor.enumerable = descriptor.enumerable || false;
+            descriptor.configurable = true;
+            if ("value" in descriptor) descriptor.writable = true;
+            Object.defineProperty(target, descriptor.key, descriptor);
+        }
+    }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+    return function (Constructor, protoProps, staticProps) {
+        if (protoProps) defineProperties(Constructor.prototype, protoProps);
+        if (staticProps) defineProperties(Constructor, staticProps);
+        return Constructor;
+    };
+}();
+
+function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+        throw new TypeError("Cannot call a class as a function");
+    }
+}
 
 window.onload = function (e) {
 
-    var a = { some: 1 };
+    var a = {some: 1};
     var i = 10;
 
-    for (var _i = 0; _i < 100; _i++) {}
+    for (var _i = 0; _i < 100; _i++) {
+    }
 
     a.some1 = 1;
     console.log(a);
